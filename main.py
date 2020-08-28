379052bccc86aaaa0b83b82663ec407fa1681eb0
Ilysa Sanchez-Perez <ims5280@psu.edu>
2020-08-28 00:05:34 +0000

    Removed in before Fahrenheit

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ temp = input("Enter a temperature in celsius: ")
 temp = float(temp)
 
 fahr = float(1.8*temp+32)
-print(str(temp) + " in Celsius is equivalent to " + str(fahr) + " in Fahrenheit.")
+print(str(temp) + " in Celsius is equivalent to " + str(fahr) + " Fahrenheit.")
